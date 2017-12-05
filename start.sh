18d508e7d145d1db77143ed0ac50f686b2a0cb22
Noah Apthorpe <apthorpe@princeton.edu>
2017-12-05 13:38:44 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Check that arguments supplied
+if [ $# -lt 2 ]
+  then
+    echo "Usage: ./start.sh output_directory pcap_filename"
+    exit 1
+fi
+
 # Check that script is running under root
 if [ "$EUID" -ne 0 ] ; then
     echo "This start script must be run with sudo."
