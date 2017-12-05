3ab354187fad54adb2ef6f37b4300ae0944e6a9f
Noah Apthorpe <apthorpe@princeton.edu>
2017-12-05 15:42:04 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,11 +6,5 @@ if [ $# -ne 6 ] ; then
     exit 1
 fi
 
-# Check that script is running under root
-if [ "$EUID" -ne 0 ] ; then
-    echo "This start script must be run with sudo."
-    exit 1
-fi
-
 python directory_monitor "$1" "$3" "$4" "$5" "$6" &
 dumpcap -P -i wlan0 -w "$1/$2.pcap" -b filesize:100000 &
