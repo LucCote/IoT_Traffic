5cc4dce5eceacb326cf071fa970f4d3c8d773261
Noah Apthorpe <apthorpe@princeton.edu>
2017-12-05 22:16:42 -0500

    Update directory_monitor.py

--- directory_monitor.py
+++ directory_monitor.py
@@ -44,7 +44,6 @@ class DirectoryMonitor:
 
     def write_to_remote(self, f):
         ssh = paramiko.SSHClient()
-        ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(self.ssh_server, username=self.ssh_username, password=self.ssh_password)
         sftp = ssh.open_sftp()
